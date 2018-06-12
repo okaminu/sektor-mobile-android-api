792f18d9f8902a7a87a3309cdad2fba7fe06ce77
vsh1ft <kastytis.ch@gmail.com>
2018-06-12 13:39:12 +0300

    workTime begin-> start

--- src/main/kotlin/lt/tlistas/loginn/mobile/android/api/type/valueobject/WorkTime.kt
+++ src/main/kotlin/lt/tlistas/loginn/mobile/android/api/type/valueobject/WorkTime.kt
@@ -1,4 +1,4 @@
 package lt.tlistas.loginn.mobile.android.api.type.valueobject
 
-data class WorkTime(val begin: Short = 0, val end: Short = 0)
+data class WorkTime(val start: Short = 0, val end: Short = 0)
 

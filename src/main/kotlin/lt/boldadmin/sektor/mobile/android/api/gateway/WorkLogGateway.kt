8abd62b4057f3894f86ba349908dfc3c1adb8b94
Andrius Degutis <andriusdgt@gmail.com>
2018-08-30 23:03:25 +0300

    BA-443-worklog: retrieves worklog interval ids

--- src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/WorkLogGateway.kt
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/WorkLogGateway.kt
@@ -4,6 +4,7 @@ import lt.boldadmin.sektor.mobile.android.api.valueobject.GpsCoordinates
 
 interface WorkLogGateway {
     fun logByLocation(gpsCoordinates: GpsCoordinates)
+    fun getIntervalIdsByCollaborator(): List<String>
     fun getProjectNameOfStartedWork(): String
     fun hasWorkStarted(): Boolean
     fun updateDescription(intervalId: String, description: String)

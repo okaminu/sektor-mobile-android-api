e449c5620b328e535eb6bbe60280baf98beb515e
Andrius Degutis <andriusdgt@gmail.com>
2018-08-31 00:56:12 +0300

    BA-443-worklog: API method reordering

--- src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/WorkLogGateway.kt
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/WorkLogGateway.kt
@@ -3,11 +3,11 @@ package lt.boldadmin.sektor.mobile.android.api.gateway
 import lt.boldadmin.sektor.mobile.android.api.valueobject.GpsCoordinates
 
 interface WorkLogGateway {
-    fun logByLocation(gpsCoordinates: GpsCoordinates)
     fun getIntervalIdsByCollaborator(): List<String>
-    fun getDescription(intervalId: String): String
     fun getProjectNameOfStartedWork(): String
     fun hasWorkStarted(): Boolean
+    fun getDescription(intervalId: String): String
     fun getDurationsSum(intervalIds: Collection<String>): Long
+    fun logByLocation(gpsCoordinates: GpsCoordinates)
     fun updateDescription(intervalId: String, description: String)
 }

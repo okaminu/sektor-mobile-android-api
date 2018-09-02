63987f16ca37bd9909c85b59d3ed3534a61cd784
Andrius Degutis <andriusdgt@gmail.com>
2018-09-02 20:14:29 +0300

    BA-443-worklog: retrieves work log endpoints

--- src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/WorkLogGateway.kt
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/WorkLogGateway.kt
@@ -1,11 +1,13 @@
 package lt.boldadmin.sektor.mobile.android.api.gateway
 
+import lt.boldadmin.sektor.mobile.android.api.type.entity.WorkLogIntervalEndpoints
 import lt.boldadmin.sektor.mobile.android.api.valueobject.GpsCoordinates
 
 interface WorkLogGateway {
     fun getIntervalIdsByCollaborator(): List<String>
     fun getProjectNameOfStartedWork(): String
     fun hasWorkStarted(): Boolean
+    fun getIntervalEndpoints(intervalId: String): WorkLogIntervalEndpoints
     fun getDescription(intervalId: String): String
     fun getDurationsSum(intervalIds: Collection<String>): Long
     fun logByLocation(gpsCoordinates: GpsCoordinates)
---
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/type/entity/WorkLogIntervalEndpoints.kt
@@ -0,0 +1,5 @@
+package lt.boldadmin.sektor.mobile.android.api.type.entity
+
+class WorkLogIntervalEndpoints {
+
+}

69d4f4fef07f0001af47bb844e27572bb20c30ea
Okaminu <aurimasdgt@gmail.com>
2019-02-21 17:45:59 +0200

    BA-742: removed description functionality

--- src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/WorklogGateway.kt
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/WorklogGateway.kt
@@ -3,13 +3,11 @@ package lt.boldadmin.sektor.mobile.android.api.gateway
 import lt.boldadmin.sektor.mobile.android.api.type.entity.WorkLogIntervalEndpoints
 import lt.boldadmin.sektor.mobile.android.api.valueobject.GpsCoordinates
 
-interface WorkLogGateway {
+interface WorklogGateway {
     fun getIntervalIdsByCollaborator(): Collection<String>
     fun getProjectNameOfStartedWork(): String
     fun hasWorkStarted(): Boolean
     fun getIntervalEndpoints(intervalId: String): WorkLogIntervalEndpoints
-    fun getDescription(intervalId: String): String
     fun getDurationsSum(intervalIds: Collection<String>): Long
     fun logByLocation(gpsCoordinates: GpsCoordinates)
-    fun updateDescription(intervalId: String, description: String)
 }

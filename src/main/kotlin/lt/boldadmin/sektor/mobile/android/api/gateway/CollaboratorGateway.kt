dc7bd32ea1b22cdaf3a2369d12d276079558860a
Okaminu <aurimasdgt@gmail.com>
2019-09-04 18:22:50 +0300

    BA-818: moved worktime to collaborator service

--- src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/CollaboratorGateway.kt
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/CollaboratorGateway.kt
@@ -1,7 +1,11 @@
 package lt.boldadmin.sektor.mobile.android.api.gateway
 
 import lt.boldadmin.sektor.mobile.android.api.valueobject.GpsCoordinates
+import lt.boldadmin.sektor.mobile.android.api.valueobject.WorkTime
 
 interface CollaboratorGateway {
+
     fun updateLocation(gpsCoordinates: GpsCoordinates)
+
+    fun getWorkTime(): WorkTime
 }

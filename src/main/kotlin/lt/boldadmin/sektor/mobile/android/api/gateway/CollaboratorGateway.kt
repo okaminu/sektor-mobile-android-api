e421a8bf213af4982b30162d1f2f823ccc735f8d
Okaminu <aurimasdgt@gmail.com>
2020-01-30 17:37:07 +0200

    BA-891: DayTime added to api along with it's tests

--- src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/CollaboratorGateway.kt
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/gateway/CollaboratorGateway.kt
@@ -1,11 +1,12 @@
 package lt.boldadmin.sektor.mobile.android.api.gateway
 
+import lt.boldadmin.sektor.mobile.android.api.valueobject.DayTime
 import lt.boldadmin.sektor.mobile.android.api.valueobject.GpsCoordinates
-import lt.boldadmin.sektor.mobile.android.api.valueobject.WorkTime
+import java.util.*
 
 interface CollaboratorGateway {
 
     fun updateCoordinates(gpsCoordinates: GpsCoordinates)
 
-    fun getWorkTime(): WorkTime
+    fun getWorkTime(): SortedSet<DayTime>
 }
---
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/valueobject/TimeRange.kt
@@ -0,0 +1,3 @@
+package lt.boldadmin.sektor.mobile.android.api.valueobject
+
+data class TimeRange(var startOfDayInMinutes: Short = 0, var endOfDayInMinutes: Short = 0)

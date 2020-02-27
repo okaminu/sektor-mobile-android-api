913ebac0a946d3e5f2003ea7d328c863105d59a9
vsh1ft <kastytis.ch@gmail.com>
2020-02-27 10:30:48 +0200

    BA-891: removed unused code

--- src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/valueobject/MinuteInterval.kt
+++ src/main/kotlin/lt/boldadmin/sektor/mobile/android/api/valueobject/MinuteInterval.kt
@@ -1,10 +1,3 @@
 package lt.boldadmin.sektor.mobile.android.api.valueobject
 
-data class MinuteInterval(var start: Short, var end: Short) {
-
-    companion object {
-        const val MIN: Short = 0
-        const val MAX: Short = 1339
-    }
-
-}
+data class MinuteInterval(var start: Short, var end: Short)
